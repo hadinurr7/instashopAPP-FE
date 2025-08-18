36bd1f27c048003eee0eb8bed9fb521f6795efd9
hadinurr7 <hadinurr7@gmail.com>
2025-08-18 20:39:24 +0700

    fix: sidebar-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import DesktopSidebar from "@/components/sidebar.component";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,16 +20,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-     <html lang="en">
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <DesktopSidebar />
-        <main style={{ marginLeft: "260px", padding: "20px" }}>
-          {children}
-        </main>
+        {children}
       </body>
     </html>
   );
